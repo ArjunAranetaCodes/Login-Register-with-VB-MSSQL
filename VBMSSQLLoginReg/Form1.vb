fbd3beef11030d0f4e68e9b50024e3941684764b
Arjun Araneta <arjunaraneta@gmail.com>
2018-09-12 08:50:31 +0800

    2018 Updates

---
+++ VBMSSQLLoginReg/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Public conn As New SqlConnection("Data Source=ALLMANKIND\MSSQLSERVER8; Database=db_vbloginreg; Integrated Security=True")
+    Public adapter As New SqlDataAdapter
+    Dim ds As DataSet
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ds = New DataSet
+        adapter = New SqlDataAdapter("insert into tbl_accounts (username, password) VALUES " &
+                                     "('" & TextBox3.Text & "','" & TextBox4.Text & "')", conn)
+        adapter.Fill(ds, "tbl_accounts")
+        TextBox3.Clear()
+        TextBox4.Clear()
+        MsgBox("User Registered!")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ds = New DataSet
+        adapter = New SqlDataAdapter("select * from tbl_accounts where username like '" & TextBox1.Text &
+                                     "' and password like '" & TextBox2.Text & "'", conn)
+        adapter.Fill(ds, "tbl_accounts")
+
+        If ds.Tables("tbl_accounts").Rows.Count > 0 Then
+            TextBox1.Clear()
+            TextBox2.Clear()
+            MsgBox("Login successful!")
+        Else
+            MsgBox("Invalid combination of username and password")
+        End If
+    End Sub
+End Class
